2d97669cf328a2e7f3ea57cd3db8de39b73a5595
Styles <stylesk11@gmail.com>
2024-02-21 12:25:19 -0800

    add component w/ ability to search

---
+++ 1-glossary/client/src/components/search.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Search = ({ handleSearch }) => {
+
+  const [term, setTerm] = React.useState('');
+
+  const handleChange = (event) => {
+    setTerm(event.target.value);
+  }
+
+  const searchTerm = () => {
+    handleSearch(term);
+    setTerm('');
+    let searchBar = document.getElementById('searchBar');
+    searchBar.value = '';
+  }
+
+
+  return(
+    <div>
+      <input type='text' id='searchBar' placeholder='search for a word...' onChange={(e) => handleChange(e)}/>
+      <button onClick={searchTerm}>SEARCH</button>
+    </div>
+  )
+}
+
+export default Search;
