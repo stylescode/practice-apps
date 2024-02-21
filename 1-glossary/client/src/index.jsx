780a41f72e08539cf817808e2c52286d04f7ee9c
Styles <stylesk11@gmail.com>
2024-02-21 12:27:04 -0800

    stub functions for search/add/edit/delete

--- 1-glossary/client/src/index.jsx
+++ 1-glossary/client/src/index.jsx
@@ -1,9 +1,42 @@
 import React from "react";
 import { render } from "react-dom";
+import axios from 'axios';
 
-render(
-  <div>
-    <p>Hello, World!</p>
-  </div>,
-  document.getElementById("root")
-);
+import WordList from './components/wordList.jsx';
+import Search from './components/search.jsx';
+import Add from './components/add.jsx';
+
+const App = () => {
+
+  const [words, setWords] = React.useState([]);
+
+  const handleSearch = (input) => {
+    console.log('you searched for ' + input);
+  }
+
+  const handleAdd = (wordToAdd, defToAdd) => {
+    console.log('adding word: ', wordToAdd);
+    console.log('adding def: ', defToAdd);
+  }
+
+  const handleEdit = () => {
+    console.log('editing word');
+  }
+
+  const handleDelete = () => {
+    console.log('deleting word');
+  }
+
+
+  return(
+    <div>
+      <p>WORDS AND DEFS</p>
+      <Search handleSearch={handleSearch}/>
+      <Add handleAdd={handleAdd}/>
+      <div>Current Dictionary:</div>
+      <WordList words={words}/>
+    </div>
+  )
+}
+
+render(<App />, document.getElementById("root"));
