e67e47a0a01b75a12175c50f9832a0281201e696
Styles <stylesk11@gmail.com>
2024-02-22 11:40:24 -0800

    change mongoose methods; fix responses

--- 1-glossary/server/db.js
+++ 1-glossary/server/db.js
@@ -1,6 +1,55 @@
 const mongoose = require("mongoose");
+mongoose.connect(`mongodb://localhost:27017/${process.env.DB_NAME}`, {useNewUrlParser: true, useUnifiedTopology: true});
+
+// const db = mongoose.connection;
+
+// db.once('open', () => {
+//   console.log('connected to database!');
+// })
 
 // 1. Use mongoose to establish a connection to MongoDB
 // 2. Set up any schema and models needed by the app
 // 3. Export the models
 // 4. Import the models into any modules that need them
+
+// use process.end.DB_NAME
+
+const entrySchema = new mongoose.Schema({
+  word: String,
+  definition: String
+})
+
+const Term = mongoose.model('Term', entrySchema);
+
+exports.save = (word, definition) => {
+  console.log('in db save');
+
+  return Term.create({ word, definition});
+
+}
+
+exports.retrieve = () => {
+  // retrieve all documents in terms collection
+  console.log('in db retrieve');
+
+  return Term.find({}).exec();
+}
+
+exports.modify = (existingWord, newDef) => {
+  console.log('in db modify');
+  // find by existingword
+  let updatedEntry = {
+    word: existingWord,
+    definition: newDef
+  }
+
+  return Term.updateOne({ word: existingWord }, { definition: newDef }).exec();
+
+}
+
+exports.remove = (wordToRemove) => {
+  console.log('in db remove');
+
+  return Term.deleteOne({ word: wordToRemove }).exec();
+
+}
--- 1-glossary/server/index.js
+++ 1-glossary/server/index.js
@@ -1,11 +1,16 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require('cors');
 const path = require("path");
+const database = require('./db.js');
 
 const app = express();
 
 // Serves up all static and generated assets in in a specified folder.
-app.use(express.static(path.join(__dirname, /* FILL ME IN */)));
+app.use(express.static(path.join(__dirname, '/client/dist')));
+console.log(__dirname);
+app.use(express.json());
+// app.use(cors());
 
 /****
  *
@@ -14,6 +19,54 @@ app.use(express.static(path.join(__dirname, /* FILL ME IN */)));
  *
  *
  */
+
+app.post('/glossary', (req, res) => {
+  console.log('made it to server side');
+  console.log(req.body);
+  database.save(req.body.wordToAdd, req.body.defToAdd)
+    .then(() => {
+      console.log('successfully added');
+      // check if code is right
+      res.status(204);
+      res.end();
+    })
+})
+
+app.get('/glossary', (req, res) => {
+  console.log('retrieving glossary');
+  database.retrieve()
+    .then((response) => {
+      console.log('glossary retrieved');
+      res.send(response);
+    })
+    .catch((error) => {
+      res.send(error);
+    })
+})
+
+app.put('/glossary', (req, res) => {
+  console.log('updating glossary');
+  console.log(req.body);
+  database.modify(req.body.existingWord, req.body.newDef)
+    .then((response) => {
+      console.log('successfully modified');
+      res.status(200);
+      res.end();
+    })
+
+})
+
+app.delete('/glossary', (req, res) => {
+  console.log('deleting item');
+  let wordToDelete = req.body.word;
+  database.remove(wordToDelete)
+    .then((response) => {
+      console.log('succesfully deleted');
+      res.status(200);
+      res.end();
+    })
+})
+
 
 app.listen(process.env.PORT);
 console.log(`Listening at http://localhost:${process.env.PORT}`);
