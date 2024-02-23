05300dde8b17a7189503cefdf8363f7f8c26b325
Styles <stylesk11@gmail.com>
2024-02-22 20:12:54 -0800

    add word id as prop

--- 1-glossary/client/src/components/word.jsx
+++ 1-glossary/client/src/components/word.jsx
@@ -1,12 +1,58 @@
 import React from 'react';
 
-const Word = ({ word }) => (
-  <div>
-    <div>{word.word}</div>
-    <div>{word.definition}</div>
-    <button class={word.id}>EDIT</button>
-    <button class={word.id}>DELETE</button>
-  </div>
-)
+let wordToEdit = '';
+let newDefinition = '';
+
+const Word = ({ word, definition, id, handleEdit, handleDelete }) => {
+
+  const handleEditClick = (event) => {
+    wordToEdit = event.target.className;
+    let modal = document.getElementById('editDialog');
+    modal.showModal();
+  }
+
+  const handleTyping = (event) => {
+    newDefinition = event.target.value;
+  }
+
+  const handleCancel = (e) => {
+    e.preventDefault();
+    wordToEdit = '';
+    newDefinition = '';
+    let modal = document.getElementById('editDialog');
+    modal.close();
+  }
+
+  const submitEdit = (e) => {
+    e.preventDefault();
+    let modal = document.getElementById('editDialog');
+    modal.close();
+    document.getElementById('editText').value = '';
+    handleEdit(wordToEdit, newDefinition);
+    wordToEdit = '';
+    newDefinition = '';
+  }
+
+  const handleDeleteClick = (event) => {
+    handleDelete(event.target.className);
+  }
+
+  return(
+    <div id='wordEntry'>
+      <div id='wordInstance'>{word}</div>
+      <div>{definition}</div>
+      <button className={word} onClick={(e) => handleEditClick(e)}>EDIT</button>
+      <button className={id} onClick={(e) => handleDeleteClick(e)}>DELETE</button>
+
+      <dialog id='editDialog'>
+        <form>
+          <textarea id='editText' placeholder='please enter a new definition' onChange={(e) => handleTyping(e)}></textarea>
+          <button onClick={(e) => submitEdit(e)}>Submit</button>
+          <button onClick={(e) => handleCancel(e)}>Cancel</button>
+        </form>
+      </dialog>
+    </div>
+  )
+}
 
 export default Word;
--- 1-glossary/client/src/components/wordList.jsx
+++ 1-glossary/client/src/components/wordList.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import Word from './wordList.jsx';
+import Word from './word.jsx';
 
-const WordList = ({ words }) => (
-
-  <div>
-    {words.forEach((word) => (
-      <Word word={word} />
-    ))}
-  </div>
-
+const WordList = ({ words, handleEdit, handleDelete}) => (
+    <div id='wordList'>
+      {words.map((word) => {
+        return <Word key={word._id} word={word.word} definition={word.definition} id={word._id} handleEdit={handleEdit} handleDelete={handleDelete} />
+      })}
+    </div>
 )
 
 export default WordList;
